b3fe76ca4a4bb0d97a335a47522ceed44ea71e79
StoneyMaroney <neuweilerthomas@gmail.com>
2024-09-10 16:23:38 +0200

    Inital Commit

---
+++ Source/BorculoStorm_IMTS/BorculoStorm_IMTS.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BorculoStorm_IMTS.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BorculoStorm_IMTS, "BorculoStorm_IMTS" );
